09a992bb97cca27bd2f0cf8bcfd1fe660a9aaf2f
Jason Reilly <jdr0887@gmail.com>
2021-06-03 17:36:34 -0400

    cleaning

--- sieve_of_atkin_serial.py
+++ sieve_of_atkin_serial.py
@@ -1,34 +1,37 @@
-def SieveOfAtkin(limit):
+import sys
+import time
+import datetime
 
+def SieveOfAtkin(limit):
     sieve = [False] * (limit + 1)
 
     for i in range(0, limit + 1):
         sieve[i] = False
 
     x = 1
-    while (x * x < limit):
+    while x * x < limit:
         y = 1
-        while (y * y < limit):
+        while y * y < limit:
 
             n = (4 * x * x) + (y * y)
-            if (n <= limit and (n % 12 == 1 or n % 12 == 5)):
+            if n <= limit and (n % 12 == 1 or n % 12 == 5):
                 sieve[n] ^= True
 
             n = (3 * x * x) + (y * y)
-            if (n <= limit and (n % 12 == 7)):
+            if n <= limit and (n % 12 == 7):
                 sieve[n] ^= True
 
             n = (3 * x * x) - (y * y)
-            if (x > y and n <= limit and (n % 12 == 11)):
+            if x > y and n <= limit and (n % 12 == 11):
                 sieve[n] ^= True
             y += 1
         x += 1
 
     x = 5
-    while (x * x < limit):
-        if (sieve[x]):
+    while x * x < limit:
+        if sieve[x]:
             y = x * x
-            while (y <= limit):
+            while y <= limit:
                 sieve[y] = False
                 y = y + x * x
         x = x + 1
@@ -41,6 +44,12 @@ def SieveOfAtkin(limit):
 
 
 if __name__ == "__main__":
+    start = time.time()
     limit = 100_000_000
+    if sys.argv.__len__() > 1:
+        limit = sys.argv[1]
+    print("limit: " + str(limit))
     SieveOfAtkin(limit)
-
+    end = time.time()
+    duration = end - start
+    print("Duration: " + str(datetime.timedelta(seconds=duration)))
