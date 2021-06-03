9e9a7c03549ffe2398fe69291be7bb31bd756cc6
Jason Reilly <jdr0887@gmail.com>
2021-06-03 17:36:06 -0400

    initial commit

---
+++ sieve_of_eratosthenes_serial.py
@@ -0,0 +1,30 @@
+import sys
+import time
+import datetime
+
+
+def SieveOfEratosthenes(n):
+
+    prime = [True for i in range(n + 1)]
+    x = 2
+    while x * x <= n:
+        if prime[x]:
+            for i in range(x * x, n + 1, x):
+                prime[i] = False
+        x += 1
+
+    # for p in range(2, n + 1):
+    #     if prime[p]:
+    #         print(p),
+
+
+if __name__ == "__main__":
+    start = time.time()
+    limit = 100_000_000
+    if sys.argv.__len__() > 1:
+        limit = sys.argv[1]
+    print("limit: " + str(limit))
+    SieveOfEratosthenes(limit)
+    end = time.time()
+    duration = end - start
+    print("Duration: " + str(datetime.timedelta(seconds=duration)))
---
+++ sieve_of_sundaram_serial.py
@@ -0,0 +1,31 @@
+import sys
+import time
+import datetime
+
+
+def SieveOfSundaram(n):
+    nNew = int((n - 1) / 2)
+
+    sieve = [0] * (nNew + 1)
+
+    for i in range(1, nNew + 1):
+        j = i
+        while (i + j + 2 * i * j) <= nNew:
+            sieve[i + j + 2 * i * j] = 1
+            j += 1
+
+    # for i in range(1, nNew + 1):
+    #     if sieve[i] == 0:
+    #         print((2 * i + 1), end=" ")
+
+
+if __name__ == "__main__":
+    start = time.time()
+    limit = 100_000_000
+    if sys.argv.__len__() > 1:
+        limit = sys.argv[1]
+    print("limit: " + str(limit))
+    SieveOfSundaram(limit)
+    end = time.time()
+    duration = end - start
+    print("Duration: " + str(datetime.timedelta(seconds=duration)))
